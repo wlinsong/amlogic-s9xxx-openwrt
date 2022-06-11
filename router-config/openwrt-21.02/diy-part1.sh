dde7ee8c1ea8b613d8bc101008de741e3d113fa5
wlinsong <103248421+wlinsong@users.noreply.github.com>
2022-06-12 01:14:27 +0800

    Update diy-part1.sh

--- router-config/openwrt-21.02/diy-part1.sh
+++ router-config/openwrt-21.02/diy-part1.sh
@@ -16,6 +16,15 @@
 # other
 # rm -rf package/lean/{samba4,luci-app-samba4,luci-app-ttyd}
 
+mkdir package/luci-app-openclash
+cd package/luci-app-openclash
+git init
+git remote add -f origin https://github.com/vernesong/OpenClash.git
+git config core.sparsecheckout true
+echo "luci-app-openclash" >> .git/info/sparse-checkout
+git pull --depth 1 origin master
+git branch --set-upstream-to=origin/master master
+
 git clone https://github.com/fw876/helloworld.git package/helloworld
 echo 'src-git helloworld https://github.com/fw876/helloworld' >>feeds.conf.default
 for i in "dns2socks" "microsocks" "ipt2socks" "pdnsd-alt" "redsocks2"; do \
